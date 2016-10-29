878ec288f93e5a30a8f9810a243ac93798f4c80e
jasonsheh <qq3039344@gmail.com>
2016-10-29 17:48:50 +0800

    Add files via upload

--- SiteScan.py
+++ SiteScan.py
@@ -8,6 +8,8 @@ import socket
 import requests
 import re
 from urllib.parse import urlparse
+import threading
+import queue
 
 
 class SiteScan:
@@ -17,14 +19,16 @@ class SiteScan:
         self.language = ''
         self.server = ''
         self.version = '1.1'
+        self.url_set = []
         self.sitemap = []
+        self.q = queue.Queue(0)
 
     def usage(self):
-        print("Usage:%s [-h|-u|-c] [--help|--version] -u || -c target...." % sys.argv[0])
+        print("Usage:%s [-h|-u|-c|-w|-d] [--help|--version] -h || --help target...." % sys.argv[0])
 
     def run(self):
         try:
-            opts, args = getopt.getopt(sys.argv[1:], "hu=:c=:w=:", ["help", "version"])
+            opts, args = getopt.getopt(sys.argv[1:], "hu:c:w:", ["help", "version"])
             for op, value in opts:
                 if op in ('--help', '-h'):
                     print('#' + '-'*60 + '#')
@@ -35,20 +39,23 @@ class SiteScan:
                     print('  -h or --help : help you know how to use this tool')
                     print('  -u : detect basic information')
                     print('  -c : crawl the site to get the sitemap')
-                    print('  -u : get whois information')
+                    print('  -w : get whois information')
+                    print('  -d : test sensitive dictionary')
                 elif op == '--version':
                     print('Current version is ' + self.version)
                 elif op == '-u':
-                    self.target = args[0]
+                    self.target = value
                     self.get_ip()
                     self.get_server()
                 elif op == '-c':
-                    self.target = args[0]
-                    print('crawl may take a while please wait...')
+                    self.target = value
                     self.site_crawl()
                 elif op == '-w':
-                    self.target = args[0]
+                    self.target = value
                     self.whois()
+                elif op == '-s':
+                    self.target = value
+                    self.sensitive_dir()
                     
         except getopt.GetoptError as e:
             print(e)
@@ -68,7 +75,7 @@ class SiteScan:
             if self.target[-1:] == '/':    # ends with slash
                 self.target = self.target[:-1]
             self.ip = socket.gethostbyname(self.target)  # get ip
-            print(' Ip address: ' + self.ip)
+            print('\n Ip address: ' + self.ip)
         except Exception as e:
             print(e)
 
@@ -138,35 +145,75 @@ class SiteScan:
                     self.sitemap[i] = self.sitemap[j]
                     self.sitemap[j] = temp
 
+    def crawler(self):
+        while not self.q.empty():
+            url = self.q.get()
+            try:
+                new_res = self.conn(url)
+            except:
+                self.q.get()
+                continue
+            res = list(set(self.get_url(new_res)))
+            for i in res:
+                if i not in self.url_set:
+                    self.url_set.append(i)
+                    self.q.put(i)
+
     # almost done need improved
     def site_crawl(self):
+        print('crawl may take a while please wait...')
+        thread_num = input('set the thread number:')
+        self.init()
         try:
-            self.init()
-
             res = self.conn(self.target)
             res = self.get_url(res)
-            for url in res:
-                try:
-                    new_res = self.conn(url)
-                except:
-                    res.remove(url)
-                    continue
-                new_res = self.get_url(new_res)
-                res += new_res
-                res = list(set(res))
+            for i in res:
+                self.url_set.append(i)
+                self.q.put(i)
+            list(set(self.url_set))
 
-            self.sitemap = self.get_dir(res)
+            threads = []
+            for i in range(int(thread_num)):
+                t = threading.Thread(target=self.crawler)
+                threads.append(t)
+            for item in threads:
+                item.setDaemon(True)
+                item.start()
+
+            # 以下不需要多线程
+            self.sitemap = self.get_dir(self.url_set)
             self.site_sort()
 
             with open('res.txt', 'w') as file:
                 for url in self.sitemap:
                     print(url)
                     file.write(url+'\n')
-                file.write('\nSensitive Dict:\n')
 
-            self.sensitive_dir()
+            # self.sensitive_dir()
         except Exception as e:
             print(e)
+
+    # get whois info
+    def whois(self):
+        if self.target.startswith('www.') or self.target.startswith('http'):
+            self.init()
+            url = self.target[11:]
+        else:
+            url = self.target
+        url = 'http://whois.alexa.cn/whois.php?u=' + url
+        r = requests.get(url)
+        # print(r.text)
+        # pattern = re.compile('(注册商.*?)')
+        # res = re.findall(pattern, r.text)
+        try:
+            res = r.text.split('域名:', 1)[1]
+            if 'No matching record' in r.text:
+                print('该域名未被注册或被隐藏')
+            else:
+                # pattern = re.compile(r'<div class="fr WhLeList-right"><div class="block ball"><span>(.*?)</span>')
+                print('' + res.replace('<br />', ''))
+        except:
+            print('查询失败')
 
     def sensitive_dir(self):
         print('\ndetecting common sensitive dictionaries...')
@@ -180,21 +227,8 @@ class SiteScan:
                         print(url)
                         file.write(url + '\n')
 
-    # get whois info
-    def whois(self):
-        if self.target.startswith('www.') or self.target.startswith('http'):
-            self.init()
-            url = self.target[11:]
-        else:
-            url = self.target
-        url = 'http://whois.alexa.cn/whois.php?u=' + url
-        r = requests.get(url)
-        if 'No matching record' in r.text:
-            print('该域名未被注册或被隐藏')
-        else:
-            # pattern = re.compile(r'<div class="fr WhLeList-right"><div class="block ball"><span>(.*?)</span>')
-            print(r.text.replace('<br />', ''))
-
+    '''def cms_verify(self):
+        self.target'''
 
 def main():
     if len(sys.argv) == 1:
