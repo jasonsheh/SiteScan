8e2448115a3268e1a7237e2e5a11081c1aede2e6
jasonsheh <qq3039344@gmail.com>
2018-01-21 00:00:52 +0800

    增加: 优化规则增加准确率

--- lib/git/gitscan.py
+++ lib/git/gitscan.py
@@ -4,6 +4,7 @@
 
 import re
 import time
+import socket
 from github import Github
 
 
@@ -13,17 +14,26 @@ reg_ip = re.compile(r"^((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}(
 
 class GitScan:
     def __init__(self, keyword):
-        self.g = Github("your key")
+        self.g = Github("a2601cbe06369ba6f81c4d0a16375a3761b24e79")
         self.keyword = keyword
         self.hash_list = []
-        self.useless_ext = ['css', 'htm', 'html']
-        self.keywords = ['password', '@163.com']
+        self.useless_ext = ['css', 'htm', 'html', 'pac', 'csv']
+        self.keywords = ['root password', 'smtp password', 'admin password']
+        self.fuzz_list = ['fuzz', 'dict', 'hack']
         self.email = ['@163.com']
 
+    # 移除无用的文件类型
     def filter(self, url):
         # 是否应该被过滤
         for ext in self.useless_ext:
             if url.endswith(ext):
+                return True
+        return False
+
+    # 移除Github上fuzz，字典相关repo
+    def remove_dict_from_repo(self, name):
+        for fuzz_keyword in self.fuzz_list:
+            if re.search(fuzz_keyword, name, re.IGNORECASE):
                 return True
         return False
 
@@ -32,9 +42,12 @@ class GitScan:
         key_code = []
         for keyword in self.keywords:
             for code in codes:
-                if re.search(keyword, code, re.IGNORECASE) or re.search(self.keyword, code, re.IGNORECASE):
-                    key_code.append(code.strip())
-        return key_code
+                for kw in keyword.split(' '):
+                    if re.search(kw, code, re.IGNORECASE) and len(code) < 300:
+                        key_code.append(code.strip())
+        if len(key_code) > 40:
+            return []
+        return list(set(key_code))
 
     def test(self):
         for kw in self.keywords:
@@ -42,22 +55,26 @@ class GitScan:
             for index, content in enumerate(resource.get_page(1)):
                 if content.sha not in self.hash_list:
                     url = content.html_url
+                    full_name = content.repository.full_name
+
                     self.hash_list.append(content.sha)
+                    if self.remove_dict_from_repo(full_name):
+                        continue
                     if self.filter(url):
                         continue
-                    # full_name = content.repository.full_name
-                    full_code = content.decoded_content.decode('utf-8')
-                    code = self.get_keyword_code(full_code)
+                    try:
+                        full_code = content.decoded_content.decode('utf-8')
+                    except socket.timeout:
+                        print(url)
+                        continue
+                    codes = self.get_keyword_code(full_code)
                     mail = re.findall(reg_mail, full_code)
                     ip = re.findall(reg_ip, full_code)
                     print(index, content.html_url)
-                    print(index, code)
-                    if mail:
-                        print(mail)
-                    if ip:
-                        print(ip)
+                    for code in codes:
+                        print('\t', code)
             time.sleep(3)
 
 
 if __name__ == '__main__':
-    GitScan('tuniu').test()
+    GitScan('huobi').test()
