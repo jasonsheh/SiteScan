e3c69a4cba873a68fd9364631e922e42baad6c44
jasonsheh <qq3039344@gmail.com>
2018-01-22 23:57:58 +0800

    增加: github api异常处理

--- lib/git/gitscan.py
+++ lib/git/gitscan.py
@@ -5,7 +5,7 @@
 import re
 import time
 import socket
-from github import Github
+from github import Github, GithubException
 
 
 reg_mail = re.compile('([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)')
@@ -16,10 +16,12 @@ class GitScan:
     def __init__(self, keyword):
         self.g = Github("a2601cbe06369ba6f81c4d0a16375a3761b24e79")
         self.keyword = keyword
+        self.search_page = 5
         self.hash_list = []
-        self.useless_ext = ['css', 'htm', 'html', 'pac', 'csv']
-        self.keywords = ['root password', 'smtp password', 'admin password']
-        self.fuzz_list = ['fuzz', 'dict', 'hack']
+        self.useless_ext = ['css', 'htm', 'html', 'pac', 'csv', 'txt', 'csv.dat', 'rules', 'md']
+        self.useful_ext = ['properties']
+        self.keywords = ['pwd', 'smtp password', 'admin password']
+        self.fuzz_list = ['fuzz', 'dict', 'hack', 'whitelist']
         self.email = ['@163.com']
 
     # 移除无用的文件类型
@@ -52,29 +54,38 @@ class GitScan:
     def test(self):
         for kw in self.keywords:
             resource = self.g.search_code(self.keyword+' '+kw, sort="indexed", order="desc")
-            for index, content in enumerate(resource.get_page(1)):
-                if content.sha not in self.hash_list:
-                    url = content.html_url
-                    full_name = content.repository.full_name
+            for page in range(1, self.search_page):
+                # You have triggered an abuse detection mechanism. Please wait a few minutes before you try again.
+                try:
+                    for index, content in enumerate(resource.get_page(page)):
+                        if content.sha not in self.hash_list:
+                            url = content.html_url
+                            full_name = content.repository.full_name
 
-                    self.hash_list.append(content.sha)
-                    if self.remove_dict_from_repo(full_name):
-                        continue
-                    if self.filter(url):
-                        continue
-                    try:
-                        full_code = content.decoded_content.decode('utf-8')
-                    except socket.timeout:
-                        print(url)
-                        continue
-                    codes = self.get_keyword_code(full_code)
-                    mail = re.findall(reg_mail, full_code)
-                    ip = re.findall(reg_ip, full_code)
-                    print(index, content.html_url)
-                    for code in codes:
-                        print('\t', code)
-            time.sleep(3)
+                            self.hash_list.append(content.sha)
+                            if self.remove_dict_from_repo(url):
+                                continue
+                            if self.filter(url):
+                                continue
+
+                            try:
+                                full_code = content.decoded_content.decode('utf-8')
+                            except socket.timeout:
+                                print('error', url)
+                                continue
+                            except GithubException.UnknownObjectException:
+                                print('error', url)
+                                continue
+                            codes = self.get_keyword_code(full_code)
+                            mail = re.findall(reg_mail, full_code)
+                            ip = re.findall(reg_ip, full_code)
+                            print(index, content.html_url)
+                            for code in codes:
+                                print('\t', code)
+                except GithubException.GithubException:
+                    time.sleep(5)
+                time.sleep(3)
 
 
 if __name__ == '__main__':
-    GitScan('huobi').test()
+    GitScan('weibo').test()
